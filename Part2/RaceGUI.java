8c267dd5dffdfaa59bb4e6508a1590dab49aa3ba
Starfall10 <103335303+Starfall10@users.noreply.github.com>
2024-04-26 07:15:42 +0100

    latest commit

--- Part2/RaceGUI.java
+++ Part2/RaceGUI.java
@@ -6,7 +6,8 @@ import java.util.concurrent.TimeUnit;
 
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
-
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 import java.awt.Color;
 import java.lang.Math;
---
+++ Part2/RunGUI.java
@@ -0,0 +1,7 @@
+package Part2;
+
+public class RunGUI {
+    public static void main(String[] args) {
+        HorseRaceGUI GUI = new HorseRaceGUI();
+    }
+}
